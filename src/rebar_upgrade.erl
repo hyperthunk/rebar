833d883ea63f71ca5a2ee495f74240892621ac7a
Tuncer Ayaz <tuncer.ayaz@gmail.com>
2011-01-25 10:40:11 +0100

    Fix whitespace and formatting

--- src/rebar_upgrade.erl
+++ src/rebar_upgrade.erl
@@ -40,25 +40,26 @@ upgrade(_Config, ReltoolFile) ->
                    "create upgrade package~n", []);
         OldVerPath ->
             %% Run checks to make sure that building a package is possible
-            {NewReleaseName, NewReleaseVer} =
-                run_checks(OldVerPath, ReltoolFile),
+            {NewReleaseName, NewReleaseVer} = run_checks(OldVerPath,
+                                                         ReltoolFile),
             Release_NewVer = NewReleaseName ++ "_" ++ NewReleaseVer,
 
             %% Save the code path prior to doing anything
             OrigPath = code:get_path(),
-            
+
             %% Prepare the environment for building the package
-            setup(OldVerPath, NewReleaseName, NewReleaseVer, Release_NewVer),
-            
+            ok = setup(OldVerPath, NewReleaseName,
+                       NewReleaseVer, Release_NewVer),
+
             %% Build the package
             run_systools(Release_NewVer, NewReleaseName),
 
             %% Clean up files that systools created
-            cleanup(Release_NewVer),
+            ok = cleanup(Release_NewVer),
 
             %% Restore original path
             true = code:set_path(OrigPath),
-            
+
             ok
     end.
 
@@ -68,27 +69,24 @@ run_checks(OldVerPath, ReltoolFile) ->
     true = release_path_check(OldVerPath),
 
     {ReleaseName, ReleaseVersion} = get_release_name(ReltoolFile),
-
     ReleaseNamePath = filename:join([".", ReleaseName]),
-
     true = release_path_check(ReleaseNamePath),
 
     {NewReleaseName, NewReleaseVer} =
         get_release_version(ReleaseName, ReleaseNamePath),
-
     {OldReleaseName, OldReleaseVer} =
         get_release_version(ReleaseName, OldVerPath),
 
     case release_name_check(NewReleaseName, OldReleaseName) of
         true ->
-            true;
+            ok;
         false ->
             ?ABORT("New and old .rel release names do not match~n", [])
     end,
 
     case release_name_check(ReleaseName, NewReleaseName) of
         true ->
-            true;
+            ok;
         false ->
             ?ABORT("Reltool and .rel release names do not match~n", [])
     end,
@@ -97,7 +95,7 @@ run_checks(OldVerPath, ReltoolFile) ->
         true ->
             ?ABORT("New and old .rel contain the same version~n", []);
         false ->
-            true
+            ok
     end,
 
     case release_version_check(ReleaseVersion, NewReleaseVer) of
@@ -106,26 +104,27 @@ run_checks(OldVerPath, ReltoolFile) ->
         false ->
             ?ABORT("Reltool and .rel versions do not match~n", [])
     end,
-    
+
     {NewReleaseName, NewReleaseVer}.
 
 get_release_name(ReltoolFile) ->
     %% expect sys to be the first proplist in reltool.config
     case file:consult(ReltoolFile) of
-        {ok, [{sys, ConfigList}| _]} ->
+        {ok, [{sys, Config}| _]} ->
             %% expect the first rel in the proplist to be the one you want
-            {rel, ReleaseName, ReleaseVersion, _} = proplists:lookup(rel, ConfigList),
-            {ReleaseName, ReleaseVersion};
+            {rel, Name, Version, _} = proplists:lookup(rel, Config),
+            {Name, Version};
         _ ->
-            ?ABORT("Rebar had an issue parsing your reltool.config~n", [])
+            ?ABORT("Failed to parse ~s~n", [ReltoolFile])
     end.
 
 get_release_version(ReleaseName, Path) ->
-    [RelFile] = filelib:wildcard(
-                  filename:join([Path, "releases", "*", ReleaseName ++ ".rel"])),
+    [RelFile] = filelib:wildcard(filename:join([Path, "releases", "*",
+                                                ReleaseName ++ ".rel"])),
     [BinDir|_] = re:replace(RelFile, ReleaseName ++ "\\.rel", ""),
     {ok, [{release, {ReleaseName1, ReleaseVer}, _, _}]} =
-        file:consult(filename:join([binary_to_list(BinDir), ReleaseName ++ ".rel"])),
+        file:consult(filename:join([binary_to_list(BinDir),
+                                    ReleaseName ++ ".rel"])),
     {ReleaseName1, ReleaseVer}.
 
 release_path_check(Path) ->
